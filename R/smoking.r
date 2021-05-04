959b87c8f55eb32f237f95d40b7d79c5dbc95a83
Dunne, Rob (Data61, Eveleigh) <Rob.Dunne@data61.csiro.au>
2021-05-04 10:02:01 +1000

    changed smoking data, docuentation

---
+++ R/smoking.r
@@ -0,0 +1,12 @@
+#' Effects of cigarette smoke on the human airway epithelial cell transcriptome
+#'
+#' A dataset containing normalized transcript measurements for 51 subjects and 22283 transcripts
+#'
+#' @format A list with rma (the transcript data) and y (the class labels):
+#' \describe{
+#'   \item{rma}{ 51 by  22283, log2 real values }
+#'   \item{y}{a character vector, "smoking" and "never-smoked"}
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE994}
+"smoking"
